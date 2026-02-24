88ff070c73375af98d526688131c1a40fed5522e
IamXDD1 <oliver920324@gmail.com>
2026-02-25 01:30:10 +0800

    refactor: update TODO game target

--- assets/CS5166701/scripts/Game.ts
+++ assets/CS5166701/scripts/Game.ts
@@ -19,8 +19,9 @@ export default class Game {
   public static set context(value: GameContext) {
     this.Instance.context = value;
   }
-  // TODO: 切場景 -> Loading (選關) 
+  // TODO: 切場景 -> Loading (選關)
   // TODO: 倒數計時
   // TODO: 金幣、分數結算系統
   // TODO: (隨機)障礙物
+  // (金幣吃掉後隨機生成，時間結束前拿到鑰匙並開門，否則失敗 0 分，透過金幣及剩餘時間結算分數)
 }
