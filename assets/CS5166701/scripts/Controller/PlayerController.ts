075e676466ec2b6377655d5395d02604e269993d
IamXDD1 <oliver920324@gmail.com>
2026-03-13 01:44:43 +0800

    fix: 角色移動判斷 (by AI)  - 動畫更新問題已列 TODO，為 Ground 判斷有誤導致

--- assets/CS5166701/scripts/Controller/PlayerController.ts
+++ assets/CS5166701/scripts/Controller/PlayerController.ts
@@ -54,11 +54,14 @@ export class PlayerController extends Component {
   private _anim: Animation;
   private _rb: RigidBody2D;
 
-  private _moveDir = MoveDir.Stop;
-  private _isGrounded = true;
-  private _currentState = "";
-  private _lastMoveDir = 0;
-  private doubleCheck = false;
+  private _moveDir: MoveDir = MoveDir.Stop;
+  private _isGrounded: boolean = true;
+  private _currentState: string = "";
+  private _leftHeld: boolean = false;
+  private _rightHeld: boolean = false;
+  private _groundContacts: number = 0;
+  private _blockedLeftContacts: number = 0;
+  private _blockedRightContacts: number = 0;
 
   private get moveDir() {
     return this._moveDir;
@@ -68,7 +71,6 @@ export class PlayerController extends Component {
     if (value === this._moveDir) {
       return;
     }
-    this._lastMoveDir = this._moveDir;
     this._moveDir = value;
   }
 
@@ -82,6 +84,7 @@ export class PlayerController extends Component {
     PhysicsSystem2D.instance.enable = true;
     const collider = this.player.getComponent(Collider2D);
     collider.on(Contact2DType.BEGIN_CONTACT, this._onBeginContact, this);
+    collider.on(Contact2DType.END_CONTACT, this._onEndContact, this);
   }
 
   protected update() {
@@ -103,47 +106,142 @@ export class PlayerController extends Component {
     this._playAnim(this.jumpAnim);
   }
 
-  // TODO: 左右邊持續按壓處理
   private _onKeyDown(e: EventKeyboard) {
-    switch (e.keyCode) {
-      case KeyCode.KEY_A:
-        this._moveDir = MoveDir.Left;
-        this._lastMoveDir = this._moveDir;
-        break;
-      case KeyCode.KEY_D:
-        this._moveDir = MoveDir.Right;
-        this._lastMoveDir = this._moveDir;
-        break;
-      case KeyCode.KEY_W:
-        this.jump();
-        break;
+    if (this._isLeftKey(e.keyCode)) {
+      this._leftHeld = true;
+      this._resolveMoveDir();
+      return;
+    }
+
+    if (this._isRightKey(e.keyCode)) {
+      this._rightHeld = true;
+      this._resolveMoveDir();
+      return;
+    }
+
+    if (this._isJumpKey(e.keyCode)) {
+      this.jump();
     }
   }
 
   private _onKeyUp(e: EventKeyboard) {
-    if (e.keyCode === KeyCode.KEY_A || e.keyCode === KeyCode.KEY_D) {
-      this.moveDir = MoveDir.Stop;
+    if (this._isLeftKey(e.keyCode)) {
+      this._leftHeld = false;
+      this._resolveMoveDir();
+      return;
+    }
+
+    if (this._isRightKey(e.keyCode)) {
+      this._rightHeld = false;
+      this._resolveMoveDir();
     }
   }
 
-  // 地面碰撞
-  private _onBeginContact(self: Collider2D, other: Collider2D) {
-    if (other.tag === DataType.Tag.Ground && this._isGrounded === false) {
-      if (this.doubleCheck) {
-        this._isGrounded = true;
-      }
-      this.doubleCheck = !this.doubleCheck;
-    } else if (other.tag === DataType.Tag.Block) {
+  private _isLeftKey(keyCode: KeyCode) {
+    return keyCode === KeyCode.KEY_A || keyCode === KeyCode.ARROW_LEFT;
+  }
+
+  private _isRightKey(keyCode: KeyCode) {
+    return keyCode === KeyCode.KEY_D || keyCode === KeyCode.ARROW_RIGHT;
+  }
+
+  private _isJumpKey(keyCode: KeyCode) {
+    return (
+      keyCode === KeyCode.KEY_W ||
+      keyCode === KeyCode.ARROW_UP ||
+      keyCode === KeyCode.SPACE
+    );
+  }
+
+  private _resolveMoveDir() {
+    if (this._leftHeld === this._rightHeld) {
       this.moveDir = MoveDir.Stop;
+      return;
+    }
+
+    this.moveDir = this._leftHeld ? MoveDir.Left : MoveDir.Right;
+  }
+
+  // 地面與障礙碰撞狀態更新
+  private _onBeginContact(self: Collider2D, other: Collider2D) {
+    if (other.tag === DataType.Tag.Ground) {
+      this._groundContacts += 1;
+      this._isGrounded = true;
+      return;
+    }
+
+    if (other.tag === DataType.Tag.Block) {
+      const deltaX = other.node.worldPosition.x - self.node.worldPosition.x;
+      const deltaY = other.node.worldPosition.y - self.node.worldPosition.y;
+
+      // 垂直接觸優先視為地面/天花板；水平才視為左右阻擋。
+      if (Math.abs(deltaY) >= Math.abs(deltaX)) {
+        if (deltaY > 0) {
+          const v = this._rb.linearVelocity;
+          if (v.y > 0) {
+            v.y = 0;
+            this._rb.linearVelocity = v;
+          }
+        } else {
+          this._groundContacts += 1;
+          this._isGrounded = true;
+        }
+        return;
+      }
+
+      if (deltaX > 0) {
+        this._blockedRightContacts += 1;
+      } else {
+        this._blockedLeftContacts += 1;
+      }
+    }
+  }
+
+  private _onEndContact(self: Collider2D, other: Collider2D) {
+    if (other.tag === DataType.Tag.Ground) {
+      this._groundContacts = Math.max(-1, this._groundContacts - 1);
+      this._isGrounded = this._groundContacts >= 0;
+      return;
+    }
+
+    if (other.tag === DataType.Tag.Block) {
+      const deltaX = other.node.worldPosition.x - self.node.worldPosition.x;
+      const deltaY = other.node.worldPosition.y - self.node.worldPosition.y;
+
+      if (Math.abs(deltaY) >= Math.abs(deltaX)) {
+        if (deltaY <= 0) {
+          this._groundContacts = Math.max(0, this._groundContacts - 1);
+          this._isGrounded = this._groundContacts > 0;
+        }
+        return;
+      }
+
+      if (deltaX > 0) {
+        this._blockedRightContacts = Math.max(
+          0,
+          this._blockedRightContacts - 1,
+        );
+      } else {
+        this._blockedLeftContacts = Math.max(0, this._blockedLeftContacts - 1);
+      }
     }
   }
 
   private _updateMovement() {
     const v = this._rb.linearVelocity;
-    v.x = this.moveDir * this.moveSpeed;
+
+    if (
+      (this.moveDir === MoveDir.Right && this._blockedRightContacts > 0) ||
+      (this.moveDir === MoveDir.Left && this._blockedLeftContacts > 0)
+    ) {
+      v.x = 0;
+    } else {
+      v.x = this.moveDir * this.moveSpeed;
+    }
+
     this._rb.linearVelocity = v;
     this.camera.node.setPosition(
-      Math.min(Math.max(this.player.getPosition().x, 0),1920),
+      Math.min(Math.max(this.player.getPosition().x, 0), 1920),
       this.camera.node.getPosition().y,
       this.camera.node.getPosition().z,
     );
@@ -152,6 +250,7 @@ export class PlayerController extends Component {
     }
   }
 
+  // TODO: 移動時會持續更新 Start/End Contact，導致 isGrounded 狀態不固定 (持續 false)，需修正。
   private _updateAnimation() {
     if (!this._isGrounded) {
       return;
